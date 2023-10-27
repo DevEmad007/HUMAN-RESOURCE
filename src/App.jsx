59d1638e1aecebc2a198f3b06c0124ff526b41f6
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-10-28 00:36:29 +0600

    routes add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Home from './Pages/Home';
+import Profile from './Pages/Profile';
+import Uploads from './Pages/Uploads';
+import { Route,Routes } from 'react-router-dom';
+import SharedLayout from './SharedLayout';
+import ProductDetails from './Components/ProducDetails/ProductDetails';
 
 const App = () => {
   return (
-    <div className=" underline">App</div>
+    <Routes>
+      <Route path='/' element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path='/products/:id' element={<ProductDetails />} />
+        <Route path='/uploads/' element={<Uploads />} />
+        <Route path='/profile/' element={<Profile />} />
+      </Route>
+    </Routes>
   );
 };
 
